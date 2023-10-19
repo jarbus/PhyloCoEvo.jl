6b025bed337c6add3595ca174f13fd9d105b3fbf
Jack <jackgarbus@gmail.com>
2023-10-19 15:00:04 -0400

    rm: Plotting code

--- src/PhyloCoEvo.jl
+++ src/PhyloCoEvo.jl
@@ -1,12 +1,10 @@
 module PhyloCoEvo
 export PhylogeneticSpeciesCreator, PhylogeneticSpecies, create_species,
        EstimationPerformer, PhylogeneticMatchMaker, TreeStatisticsMetric,
-       OutcomeScalarFitnessEvaluator, OutcomeScalarFitnessEvaluation,
-       plot_per_distance_errors
+       OutcomeScalarFitnessEvaluator, OutcomeScalarFitnessEvaluation
 include("./species.jl")
 include("./evaluation.jl")
 include("./metrics.jl")
 include("./estimation-performer.jl")
 include("./matchmaker.jl")
-include("./plots.jl")
 end
