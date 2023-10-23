78b16542538268ee9f5ad7825979deafec2ed4e0
Jack <jackgarbus@gmail.com>
2023-10-23 16:26:31 -0400

    test: test interaction results

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
 
 @testset "PhyloCoEvo.jl" begin
     # Write your tests here.
-    #include("x/ng/1/config.jl")
+    include("x/ng/1/config.jl")
     include("sorting-networks.jl")
     include("x/sn/1/config.jl")
 end
