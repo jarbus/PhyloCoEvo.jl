3899c76f70f1295a7ecc90728462bf07e3561066
jarbus <jarbus@tutanota.com>
2023-11-06 11:24:27 -0500

    feat: OutcomeNSGAIIEvaluat{ion,or}

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -1,9 +1,10 @@
-export OutcomeScalarFitnessEvaluation, OutcomeScalarFitnessEvaluator
+export OutcomeScalarFitnessEvaluation, OutcomeScalarFitnessEvaluator, OutcomeNSGAIIEvaluation, OutcomeNSGAIIEvaluator
 
 using DataStructures: SortedDict
 using CoEvo.Species: AbstractSpecies
 using CoEvo.Individuals: Individual
 using CoEvo.Evaluators: evaluate
+using CoEvo.Evaluators.NSGAII: NSGAIIRecord, NSGAIIEvaluation, NSGAIIEvaluator
 using CoEvo.Evaluators.ScalarFitness: ScalarFitnessEvaluator, ScalarFitnessEvaluation, ScalarFitnessRecord
 
 struct OutcomeScalarFitnessEvaluation <: CoEvo.Evaluators.Evaluation
@@ -12,9 +13,23 @@ struct OutcomeScalarFitnessEvaluation <: CoEvo.Evaluators.Evaluation
     outcomes::Dict{Int, Dict{Int, Float64}}
 end
 
+struct OutcomeNSGAIIEvaluation <: Evaluation
+    species_id::String
+    records::Vector{NSGAIIRecord}
+    outcomes::Dict{Int, Dict{Int, Float64}}
+end
+
 Base.@kwdef struct OutcomeScalarFitnessEvaluator <: CoEvo.Evaluators.Evaluator 
     maximize::Bool = true
     epsilon::Float64 = 1e-6
+end
+
+Base.@kwdef struct OutcomeNSGAIIEvaluator <: Evaluator 
+    maximize::Bool = true
+    perform_disco::Bool = true
+    max_clusters::Int = -1
+    function_minimums::Union{Vector{Float64}, Nothing} = nothing
+    function_maximums::Union{Vector{Float64}, Nothing} = nothing
 end
 
 function CoEvo.Evaluators.evaluate(
@@ -35,6 +50,28 @@ function CoEvo.Evaluators.evaluate(
     evaluation = OutcomeScalarFitnessEvaluation(species.id, scalar_fitness_evaluation.records, outcomes)
     return evaluation
 end
+
+function CoEvo.Evaluators.evaluate(
+    evaluator::OutcomeNSGAIIEvaluator,
+    random_number_generator::AbstractRNG,
+    species::AbstractSpecies,
+    outcomes::Dict{Int, SortedDict{Int, Float64}})
+    """Wrapper around NSGAIIEvaluator to create OutcomeNSGAIIEvaluation
+    """
+    nsgaii_evaluator = NSGAIIEvaluator(
+        maximize = evaluator.maximize,
+        perform_disco = evaluator.perform_disco,
+        max_clusters = evaluator.max_clusters,
+        function_minimums = evaluator.function_minimums,
+        function_maximums = evaluator.function_maximums
+    )
+    nsgaii_evaluation = evaluate(
+        nsgaii_evaluator, random_number_generator, species, outcomes
+    )
+    evaluation = OutcomeNSGAIIEvaluation(species.id, nsgaii_evaluation.records, outcomes)
+    return evaluation
+end
+
 
 function CoEvo.Replacers.replace(
     replacer::CoEvo.Replacers.Generational.GenerationalReplacer,
